a6af9b361b9cf26cf3b18682dbd93e55fc1d0d20
aurora muggianu <auroraemu@gmail.com>
2024-03-05 11:19:21 +0100

    commit message

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def about
+  end
+  def contact
+  end
+  def youth_illustrations
+  end
+  def small_hoomans
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get "/", to: "homepage#home", as: "root"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/about', to: 'pages#about', as: 'about'
+  get '/contact', to: 'pages#contact', as: 'contact'
+  get '/youth_illustrations', to: 'pages#youth_illustrations', as: 'youth_illustrations'
+  get '/small_hoomans', to: 'pages#small_hoomans', as: 'small_hoomans'
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
