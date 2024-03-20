e61744a83e09f46e811117d87bd1959668e0fdbc
aurora muggianu <auroraemu@gmail.com>
2024-03-20 15:52:21 +0100

    youth_illu ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,4 +9,6 @@ class PagesController < ApplicationController
   end
   def portraits
   end
+  def pages
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/youth_illustrations', to: 'pages#youth_illustrations', as: 'youth_illustrations'
   get '/small_hoomans', to: 'pages#small_hoomans', as: 'small_hoomans'
   get '/portraits', to: 'pages#portraits', as: 'portraits'
+  get '/places', to: 'pages#places', as: 'places'
   # Defines the root path route ("/")
   # root "articles#index"
 end
